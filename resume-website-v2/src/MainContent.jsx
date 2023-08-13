be3eeb01358ef69e4b719cb66620f09ae363c42b
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2023-08-13 17:11:01 -0400

    First page ready

---
+++ resume-website-v2/src/MainContent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Socials from "./Socials";
+
+const MainContent = () => {
+    return (
+        <div className='MainContent'>
+            <h1>Hello, I am Mariia!</h1>
+            <p>An enthusiastic software engineer and web developer.</p>
+            <Socials />
+        </div>
+    )
+}
+
+export default MainContent;
---
+++ resume-website-v2/src/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <a href='#'><img className='logo-img' src='./logo1.png'></img></a>
+            <a href='#'><img className='bars-img' src='./bars-solid.svg'></img></a>
+        </nav>
+    )
+}
+
+export default Navbar;
