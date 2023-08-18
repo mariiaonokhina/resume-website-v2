de0cbb19c91ffca05ac821e59490369a178b902a
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2023-08-17 21:18:56 -0400

    Finished landing page

--- resume-website-v2/src/App.jsx
+++ resume-website-v2/src/App.jsx
@@ -2,8 +2,12 @@ import './App.css'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import MainContent from './MainContent'
+import { useEffect } from 'react'
 
 function App() {
+  useEffect(() => {
+    document.title = "Mariia's Website";
+  }, []);
 
   return (
     <div className="App">
